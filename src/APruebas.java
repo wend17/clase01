54abe905f75a85fc3f63f302f075e1c9a86b6896
wendy <wendyvillazana@gmail.com>
2022-04-22 17:48:20 -0500

    clase05 v5

--- src/APruebas.java
+++ src/APruebas.java
@@ -1,17 +1,31 @@
+import java.util.Locale;
+
 public class APruebas {
     public static void main(String[] args) {
+        String nombre = "Andres";
 
-        String numero ="50";
-        int numeroInt = Integer.parseInt(numero);
-        System.out.println("numeroInt = " + numeroInt);
+        System.out.println("caracteres= " + nombre.length());
+        System.out.println("convertir en mayúscula = " + nombre.toUpperCase());
+        System.out.println("convertir en minúscula= " + nombre.toLowerCase());
+        System.out.println("comparación = " + nombre.equals("Andres"));
+        System.out.println("comparación2 = " + nombre.equals("andres"));
+        System.out.println("comparación3 = " + nombre.equalsIgnoreCase("andres"));
+        System.out.println("comparación4 = " + nombre.compareTo("andres")); // igual al equal, si sale 0, es
+        System.out.println("traer carácter = " + nombre.charAt(0));
+        System.out.println("desde el carácter = " + nombre.substring(1));
+        System.out.println("desde, sin incluir el hasta = " + nombre.substring(1,4));
 
-        String realStr =" 98765.43e-3";
-        double realDouble = Double.parseDouble(realStr);
-        System.out.println("realDouble = " + realDouble);
-        
-        String logicoStr = "true";
-        boolean logicoBoolean = Boolean.parseBoolean(logicoStr);
-        System.out.println("logicoBoolean = " + logicoBoolean);
+        String trabalenguas = "trabalenguas";
+        System.out.println("remplazo un caracter por otro = " + trabalenguas.replace("a","."));
+        System.out.println("primera posición del caracter o palabra= " + trabalenguas.indexOf('a')); // acepta string y caracter y
+        System.out.println("trabalenguas.lastIndexOf('a') = " + trabalenguas.lastIndexOf('a')); // si no existe el caracter la posicion será negativa
+        System.out.println(" acepta solo string ,respuesta es boolean  = " + trabalenguas.contains("t"));
+        System.out.println(" empieza con ......= " + trabalenguas.startsWith("lenguas"));
+        System.out.println("termina con tr = " + trabalenguas.endsWith("tr"));
+        System.out.println("  trabalenguas  ".trim()); // para quitar espacios
 
+        String archivo = "imagen.html";
+        int i = archivo.lastIndexOf(".");
+        System.out.println("obteniendo extención = " + archivo.substring(i+1));
     }
 }
